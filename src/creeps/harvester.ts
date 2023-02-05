53b2099146eacd6caffd23da461a6daffe896f05
ben1289 <ben_1289@163.com>
2023-02-05 22:53:32 +0800

    修改工作状态转换逻辑

--- src/creeps/harvester.ts
+++ src/creeps/harvester.ts
@@ -11,13 +11,15 @@ export default class Harvester extends Creep {
   public run(): void {
     for (const harvester of this.creeps) {
       if (this.renewTick(harvester)) return;
-      const source = Game.getObjectById('5bbcae0a9099fc012e638590' as Id<_HasId>) as Source;
-      if (harvester.store.getFreeCapacity() > 0) {
-        // 装载量有空余 前去挖矿
-        if (harvester.harvest(source) === ERR_NOT_IN_RANGE) {
-          harvester.moveTo(source, { visualizePathStyle: { stroke: '#ffff00' } });
-        }
-      } else {
+      if (harvester.memory.working && harvester.store[RESOURCE_ENERGY] === 0) {
+        harvester.memory.working = false;
+        harvester.say('⛏️ 去挖矿');
+      }
+      if (!harvester.memory.working && harvester.store.getFreeCapacity() === 0) {
+        harvester.memory.working = true;
+        harvester.say('⬇️ 去卸货');
+      }
+      if (harvester.memory.working) {
         // 装载量满了 前去卸矿
         let targets = harvester.room.find(FIND_STRUCTURES, {
           filter: (structure: StructureSpawn | StructureExtension) =>
@@ -37,6 +39,12 @@ export default class Harvester extends Creep {
         } else {
           // 没有能存矿的建筑 前去首个 spawn 处等待
           harvester.moveTo(this.spawns[0], { visualizePathStyle: { stroke: '#ffffff' } });
+        }
+      } else {
+        const source = Game.getObjectById('5bbcae0a9099fc012e638590' as Id<_HasId>) as Source;
+        // 装载量有空余 前去挖矿
+        if (harvester.harvest(source) === ERR_NOT_IN_RANGE) {
+          harvester.moveTo(source, { visualizePathStyle: { stroke: '#ffff00' } });
         }
       }
     }
