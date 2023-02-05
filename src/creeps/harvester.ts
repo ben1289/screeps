074a886283771af183504e0f8eff8c784d7ce2d8
ben1289 <ben_1289@163.com>
2023-02-05 17:10:44 +0800

    将扩展的优先级提高

--- src/creeps/harvester.ts
+++ src/creeps/harvester.ts
@@ -15,11 +15,15 @@ export default class Harvester extends Creep {
         }
       } else {
         // 装载量满了 前去卸矿
-        const targets = harvester.room.find(FIND_STRUCTURES, {
+        let targets = harvester.room.find(FIND_STRUCTURES, {
           filter: (structure: StructureSpawn | StructureExtension) =>
             [STRUCTURE_SPAWN, STRUCTURE_EXTENSION].includes(structure.structureType) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
         });
+        // 将扩展的优先级提高
+        const extension = targets.filter(structure => structure.structureType === STRUCTURE_EXTENSION);
+        targets = extension.length > 0 ? extension : targets;
+
         if (targets.length > 0) {
           // 有能存矿的建筑 前去存矿
           if (harvester.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
