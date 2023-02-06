9f9aa72c3dfde2bc56dea46319bdcc7ab00b9a05
ben1289 <ben_1289@163.com>
2023-02-06 22:21:37 +0800

    调整优先顺序

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import Repairer from './creeps/repairer';
 export const loop = ErrorMapper.wrapLoop(() => {
   for (const roomsKey in Game.rooms) {
     const room = Game.rooms[roomsKey];
-    const harvester = new Harvester(room);
-    harvester.run();
-    const updater = new Updater(room);
-    updater.run();
-    const builder = new Builder(room);
-    builder.run();
     const repairer = new Repairer(room);
     repairer.run();
+    const builder = new Builder(room);
+    builder.run();
+    const updater = new Updater(room);
+    updater.run();
+    const harvester = new Harvester(room);
+    harvester.run();
   }
 
   // Automatically delete memory of missing creeps
