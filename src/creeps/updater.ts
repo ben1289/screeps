157388c7d3bc6c20e49fbdb06076cfdd265499f4
ben1289 <ben_1289@163.com>
2023-02-07 23:10:45 +0800

    优先去container中拿去能量

--- src/creeps/updater.ts
+++ src/creeps/updater.ts
@@ -25,9 +25,19 @@ export default class Updater extends CreepBase {
           creep.moveTo(this.room.controller as StructureController, { visualizePathStyle: { stroke: '#ff0000' } });
         }
       } else {
-        const source = Game.getObjectById('5bbcac8a9099fc012e635a89' as Id<_HasId>) as Source;
-        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, { visualizePathStyle: { stroke: '#ffff00' } });
+        const targets = this.room.find(FIND_STRUCTURES, {
+          filter: structure =>
+            structure.structureType === STRUCTURE_CONTAINER && structure.store.getCapacity(RESOURCE_ENERGY) > 0
+        });
+        if (targets.length > 0) {
+          if (creep.withdraw(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffff00' } });
+          }
+        } else {
+          const source = Game.getObjectById('5bbcac8a9099fc012e635a89' as Id<_HasId>) as Source;
+          if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(source, { visualizePathStyle: { stroke: '#ffff00' } });
+          }
         }
       }
     }
