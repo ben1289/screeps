8e1cbffe55c6e93d76edd0c3a8ca0324c34ee31c
ben1289 <ben_1289@163.com>
2023-02-14 23:32:04 +0800

    如果有container则放弃运输只专心采集

--- src/creeps/harvester.ts
+++ src/creeps/harvester.ts
@@ -1,30 +1,60 @@
 import CreepBase from './creepBase';
+import { getObjectById } from '../utils';
 
 /**
  * 采集者
  */
 export default class Harvester extends CreepBase {
+  private readonly sourceContainer;
+
   public constructor(room: Room, maximum = 3) {
     super(room, 'harvester');
-    this.generate(maximum);
+    this.sourceContainer = this.room.memory.sourceContainer;
+    if (this.sourceContainer.length > 0) {
+      this.generate(this.sourceContainer.length);
+    } else {
+      this.generate(maximum);
+    }
     this.run();
   }
 
   private run(): void {
-    for (const creep of this.creeps) {
-      if (this.renewTick(creep)) continue;
+    for (let i = 0; i < this.creeps.length; i++) {
+      const creep = this.creeps[i];
 
-      if (!creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
-        creep.memory.working = true;
-      }
-      if (creep.memory.working && creep.store.getFreeCapacity() === 0) {
-        creep.memory.working = false;
-      }
-
-      if (creep.memory.working) {
-        this.toHarvest(creep);
+      if (this.sourceContainer.length > 0) {
+        const source = getObjectById<Source>(this.sourceContainer[i].sourceId);
+        const container = getObjectById<StructureContainer>(this.sourceContainer[i].containerId);
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(container);
+        }
+        if (creep.store.getFreeCapacity() === 0) {
+          if (creep.store.getUsedCapacity() <= container.store.getFreeCapacity()) {
+            creep.drop(RESOURCE_ENERGY);
+          } else {
+            const links = this.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_LINK } });
+            for (const link of links) {
+              if (creep.pos.inRangeTo(link, 1)) {
+                creep.transfer(link, RESOURCE_ENERGY);
+              }
+            }
+          }
+        }
       } else {
-        this.toStore(creep);
+        if (this.renewTick(creep)) continue;
+
+        if (!creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
+          creep.memory.working = true;
+        }
+        if (creep.memory.working && creep.store.getFreeCapacity() === 0) {
+          creep.memory.working = false;
+        }
+
+        if (creep.memory.working) {
+          this.toHarvest(creep);
+        } else {
+          this.toStore(creep);
+        }
       }
     }
   }
