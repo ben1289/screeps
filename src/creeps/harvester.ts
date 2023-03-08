bb02bcd1bf3f82f2aec135554dad23525e83be0b
ben1289 <ben_1289@163.com>
2023-03-08 23:14:42 +0800

    使用store 调整逻辑

--- src/creeps/harvester.ts
+++ src/creeps/harvester.ts
@@ -1,48 +1,35 @@
 import CreepBase from './creepBase';
-import { getObjectById } from '../utils';
+import { mySource, linksNearSource, containersNearSource } from '../store';
 
 /**
  * 采集者
  */
 export default class Harvester extends CreepBase {
-  private readonly sourceContainer;
+  protected readonly sources;
 
   public constructor(room: Room, maximum = 3) {
     super(room, 'harvester');
-    this.sourceContainer = this.room.memory.sourceContainer ?? [];
-    if (this.sourceContainer.length > 0) {
-      this.generate(this.sourceContainer.length);
+    this.sources = mySource.get(room.name) ?? [];
+    if (this.sources.length > 0) {
+      this.generate(this.sources.length);
+      this.run(false);
     } else {
       this.generate(maximum);
+      this.run(true);
     }
-    this.run();
   }
 
-  private run(): void {
+  /**
+   * 执行工作
+   * @param transportMode 是否启用运输模式
+   * @private
+   */
+  private run(transportMode: boolean): void {
     for (let i = 0; i < this.creeps.length; i++) {
       const creep = this.creeps[i];
 
-      if (this.sourceContainer.length > 0) {
-        const source = getObjectById<Source>(this.sourceContainer[i].sourceId);
-        const container = getObjectById<StructureContainer>(this.sourceContainer[i].containerId);
-        if (creep.pos.isEqualTo(container)) {
-          creep.harvest(source);
-        } else {
-          creep.moveTo(container);
-        }
-        if (creep.store.getFreeCapacity() === 0) {
-          if (creep.store.getUsedCapacity() <= container.store.getFreeCapacity()) {
-            creep.drop(RESOURCE_ENERGY);
-          } else {
-            const links = this.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_LINK } });
-            for (const link of links) {
-              if (creep.pos.isNearTo(link)) {
-                creep.transfer(link, RESOURCE_ENERGY);
-              }
-            }
-          }
-        }
-      } else {
+      if (transportMode) {
+        // 兼顾运输模式
         if (this.renewTick(creep)) continue;
 
         if (!creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
@@ -56,6 +43,34 @@ export default class Harvester extends CreepBase {
           this.toHarvest(creep);
         } else {
           this.toStore(creep);
+        }
+      } else {
+        // 仅采集模式
+        const source = this.sources[i];
+
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
+
+        if (creep.store.getFreeCapacity() === 0) {
+          const link = (linksNearSource.get(source.id) ?? []).find(
+            linkNearSource => creep.store.getUsedCapacity() <= (linkNearSource.store.getFreeCapacity() ?? 0)
+          );
+          if (link) {
+            if (creep.transfer(link, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+              creep.moveTo(link);
+            }
+            return;
+          }
+
+          const container = (containersNearSource.get(source.id) ?? []).find(
+            containerNearSource => creep.store.getUsedCapacity() <= (containerNearSource.store.getFreeCapacity() ?? 0)
+          );
+          if (container) {
+            if (creep.transfer(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+              creep.moveTo(container);
+            }
+          }
         }
       }
     }
--- src/creeps/transporter.ts
+++ src/creeps/transporter.ts
@@ -1,17 +1,18 @@
 import { sortBy } from 'lodash-es';
 import CreepBase from './creepBase';
+import { mySource, containersNearSource, linksNearSource, otherLinks } from '../store';
 
 /**
  * 运输工
  */
 export default class Transporter extends CreepBase {
-  private readonly containers: StructureContainer[];
+  protected readonly room;
 
   public constructor(room: Room, maximum = 1) {
     super(room, 'transporter');
+    this.room = room;
 
-    this.containers = room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_CONTAINER } });
-    if (this.containers.length > 0) {
+    if (containersNearSource.size > 0 || linksNearSource.size > 0) {
       this.generate(maximum);
       this.run();
     } else {
@@ -23,7 +24,13 @@ export default class Transporter extends CreepBase {
     for (const creep of this.creeps) {
       if (this.renewTick(creep)) continue;
 
-      const bestContainer = sortBy(this.containers, container => container.store.getFreeCapacity())[0];
+      const sources = mySource.get(this.room.name) ?? [];
+      const containers: (StructureContainer | StructureLink)[] = [...(otherLinks.get(this.room.name) ?? [])];
+      for (const source of sources) {
+        containers.push(...(containersNearSource.get(source.id) ?? []));
+      }
+
+      const bestContainer = sortBy(containers, container => container.store.getFreeCapacity())[0];
       if (!creep.memory.working && creep.store.getUsedCapacity() === 0) {
         creep.memory.working = true;
         creep.say('一滴不剩了 去拿货');
